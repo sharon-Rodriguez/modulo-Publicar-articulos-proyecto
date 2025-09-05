7806002464936a29095c9d8fc352532fca6f839b
Sharon Rodriguez <sharo2599@gmail.com>
2025-09-04 23:24:34 -0500

    Crear clases iniciales para Articulos y Usuario en Model, Repository y Controller

---
+++ src/main/java/com/PublicarArticulos/GestorArticulos/Model/Usuario.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.PublicarArticulos.GestorArticulos.Model;
+
+/**
+ *
+ * @author USER
+ */
+import jakarta.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "Usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUsuario;
+
+    private String nombre;
+
+    private String correo;
+
+    private String contraseña;
+
+    private String ciudad;
+
+    private Date fechaRegistro;
+
+    private Double reputacion;
+
+    // Getters y Setters (para acceder a los atributos)
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public Double getReputacion() {
+        return reputacion;
+    }
+
+    public void setReputacion(Double reputacion) {
+        this.reputacion = reputacion;
+    }
+}
+    
+}
