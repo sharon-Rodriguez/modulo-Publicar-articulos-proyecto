4a8f2232436a33c48f62b8fac919353874b3448f
Sharon Rodriguez <sharo2599@gmail.com>
2025-09-04 23:47:26 -0500

    Agregar controladores CRUD para Usuario y Articulo

---
+++ src/main/java/com/PublicarArticulos/GestorArticulos/Controller/Usuarios.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.PublicarArticulos.GestorArticulos.Controller;
+
+/**
+ *
+ * @author USER
+ */
+public class Usuarios {
+    
+}
